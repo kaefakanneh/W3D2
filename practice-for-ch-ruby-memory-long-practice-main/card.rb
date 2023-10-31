5f444798eb16eab27c4c31043123ad7a6323dee3
JhonJSC182 <jhonjsc182@hotmail.com>
2023-10-31 15:34:49 -0400

    added classes & methods(initialize, hide) to card class

--- practice-for-ch-ruby-memory-long-practice-main/card.rb
+++ practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -1,3 +1,14 @@
 class Card
 
+    attr_accessor :face_up
+
+    def initialize
+        @face_value = face_value
+        @face_up = false
+    end
+
+
+    def hide
+        face_up = false
+    end
 end
