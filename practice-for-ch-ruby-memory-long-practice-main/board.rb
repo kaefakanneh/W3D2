d097b84f0d42ce664b710ffd38c32712991160bd
Kaefa Kanneh <kaefakanneh@hotmail.com>
2023-10-31 16:48:39 -0400

    initialize method for Board class was written

--- practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,3 +1,52 @@
 class Board
+  def initialize
+    @grid = Array.new(4) { Array.new(4) }
 
+  end
+
+  # def [](pos)
+  #   row, col = pos
+
+  #   @grid[row][col]
+  # end
+
+  # def []=(pos, value)
+  #   row, col = pos
+  #   @grid[row][col] = value
+  # end
+
+  # def populate
+    
+  # end
+
+  # def render
+
+  # end
+
+  # def won?
+
+  # end
+
+  # def reveal
+  #   @game.guessed_pos
+
+  # end
+
+  def update_cards
+    puts "updating the cards"
+    self.repopulate
+    
+    #a buncch of other methods 
+    @cards.each do |card|
+      @grid << card
+      card.flip
+
+      #a bunch of other methods
+
+      card.hide
+    end
+    
+  end
 end
+
+# board = Board.new
--- practice-for-ch-ruby-memory-long-practice-main/card.rb
+++ practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -2,7 +2,7 @@ class Card
 
     attr_accessor :face_up
 
-    def initialize
+    def initialize(face_value)
         @face_value = face_value
         @face_up = false
     end
@@ -10,5 +10,17 @@ class Card
 
     def hide
         face_up = false
+    end
+
+    def reveal
+        face_up = true
+    end
+
+    def to_s
+   
+    end
+
+    def ==
+
     end
 end
